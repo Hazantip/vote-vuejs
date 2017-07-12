d7d0745d28185c801b550db31d45992ef1669190
hazantip <hazantip@rabbi.co.il>
2017-07-12 19:28:58 +0300

    end of v1

--- src/main.js
+++ src/main.js
@@ -13,46 +13,77 @@ function DOMLoaded() {
 	Vue.use(VueTouch);
 
 	// Hammer Pan Events: pan, panstart, panmove, panend, pancancel, panleft, panright, panup, pandown
+	// TODO(done): image state styles
+	// TODO(done): image slider state styles
+	// TODO(done): image slider elements
+	// TODO(done): image slider implement with results
+	// TODO: stylize handle
+	// TODO: set default 50%
+
+	// - NEW!!!
+	// TODO: make horizontal, start left (if have time make right too)
+	// TODO: add feature stars
+	// TODO: add feature like/dislike
+	// TODO: add vote button
+	// TODO: show result with fun animation after vote
 
 	const sliderDefaults = {
-		'enablePlay': true,				// - enable/disable events
-		'enableSwipeX': true, 			// - default false
-		'enableSwipeY': true,			// - not used
-		'value': '0%',
+		'enablePlay': false,			// - enable/disable events
 		'startPoint': 'bottom',			// - define direction from the point: bottom, top, left, right
+		'value': 0.5,
 		'scale': {
+			'element': '.slider-scale__line',
 			'height': null				// - total scale height
 		},
-		'dot': {
+		'handle': {
 			'position': {
-				'y': 0					// - last dot position
+				'y': 0					// - last handle position
 			}
 		},
-		'style': {}						// - inline css object for dot
+		'timeline': {
+			'initialResultsDelay': 1000
+		},
+		'style': {}						// - inline css object for handle
 	};
 	const slider = new Vue({
 		'el': '#slider',
 		'data': {
 			...sliderDefaults,			// - default params
-			'enablePlay': false,			// - initially overrides default value
-			'timeline': {					// - not user
-				'showHintDelay': 300,
-				'hideHintDelay': 3000,
-				'questionLeaveDuration': 300,
-				'showResultsDuration': 3000
+			'image': {
+				'src': '/assets/images/singer.jpg'
+			},
+			'results': {
+				'active': false,				// -
+				'value': 0.7				// -
 			}
 		},
 		'created': function() {
-			//console.log('created');
+			//console.log('created', this);
+
+			// - set scale size
+			const height = this.querySelector(this.scale.element).clientHeight;
+			this.setScale({ height });
+
+			// - set initial value
+			if (!this.results.active) {
+				this.setPosition({ 'type': 'force', 'positionY': height * 0.5 });
+			}
+
 		},
 		'beforeMount': function() {
-			//console.log('beforeMount');
+			//console.log('beforeMount', this);
 		},
 		'mounted': function() {
 			//console.log('mounted');
-			this.setScale({
-				'height': this.$refs.line.clientHeight
-			});
+
+			// - set initial value
+			if (this.results.active) {
+				this.setPosition({ 'type': 'force', 'positionY': this.scale.height * this.results.value });
+			}
+			// - set enable play
+			setTimeout(() => {
+				this.enablePlay = true;
+			}, this.timeline.initialResultsDelay);
 		},
 		'beforeUpdate': function() {
 			//console.log('beforeUpdate');
@@ -64,17 +95,17 @@ function DOMLoaded() {
 			'onPanMove': function ({ type, deltaY }) {
 				const positionY =
 						this.startPoint === 'bottom'
-							? this.dot.position.y + -(deltaY)
-							: this.dot.position.y + deltaY;
+							? this.handle.position.y + -(deltaY)
+							: this.handle.position.y + deltaY;
 
 				this.setPosition({ type, positionY });
 
 			},
 			'onPanEnd': function ({ deltaY }) {
-				this.dot.position.y =
+				this.handle.position.y =
 						this.startPoint === 'bottom'
-							? this.dot.position.y + -(deltaY)
-							: this.dot.position.y + deltaY;
+							? this.handle.position.y + -(deltaY)
+							: this.handle.position.y + deltaY;
 			},
 			'onTap': function ({ type, srcEvent }) {
 				const offsetY = srcEvent.offsetY || srcEvent.layerY;
@@ -85,32 +116,40 @@ function DOMLoaded() {
 
 				this.setPosition({ type, positionY });
 			},
-			'setPosition': function({ type, positionY }) {
-				const height = slider.$refs.line.clientHeight;
+			'setPosition': function({ type, ...rest }) {
+				const height = this.querySelector(this.scale.element).clientHeight;
 				this.setScale({ height }); // important for case when was resized
 
-				if (positionY < 0 || positionY > height) {
-					return false;
+				//console.log(this.scale.element, this.scale.element.clientHeight);
+
+				let { positionY } = rest;
+				if (positionY < 0) {
+					positionY = 0;
+				}
+				if (positionY > height) {
+					positionY = height;
 				}
 
 				this.setValue({ positionY, height });
 
-				if (type === 'tap') {
-					this.style = {
-						'transform': `translateY(${this.startPoint === 'bottom' ? -positionY : positionY}px)`
-					};
-					this.dot.position.y = positionY;
-				} else if (type === 'panmove') {
-					this.style = {
-						'transform': `translateY(${this.startPoint === 'bottom' ? -positionY : positionY}px)`
-					};
+				this.style = {
+					'transform': `translateY(${this.startPoint === 'bottom' ? -positionY : positionY}px)`,
+					'transition': type === 'force' ? '' : 'none',
+					'transitionDelay': type === 'force' ? `${this.timeline.initialResultsDelay}ms` : ''
+				};
+
+				if (type === 'tap' || type === 'force') {
+					this.handle.position.y = positionY;
 				}
 			},
 			'setScale': function(data) {
 				this.scale = Object.assign(this.scale, data, {});
 			},
 			'setValue': function ({ positionY, height }) {
-				this.value = `${Math.round(((positionY / height) * 100))}%`;
+				this.value = positionY / height;
+			},
+			'printValue': function () {
+				return Math.round(this.value * 100);
 			}
 		}
 	});
